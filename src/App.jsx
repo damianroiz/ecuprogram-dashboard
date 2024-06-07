4debec8e208eb731cc2618bbcaac56a7ec424e8d
damianx989 <damianx989@gmail.com>
2024-06-07 21:27:57 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Dashboard from "./pages/Dashboard.jsx"
+import Bookings from "./pages/Bookings.jsx"
+import Cabins from "./pages/Cabins.jsx"
+import Users from "./pages/Users.jsx"
+import Settings from "./pages/Settings.jsx"
+import Account from "./pages/Account.jsx"
+import Login from "./pages/Login.jsx"
+import PageNotFound from './pages/PageNotFound.jsx'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="booking" element={<Bookings />} />
+        <Route path="cabins" element={<Cabins />} />
+        <Route path="users" element={<Users />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="account" element={<Account />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  
+  )   
+}
+
+
+export default App
